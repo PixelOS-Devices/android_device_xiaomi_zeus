eed9c0949e0db6b6524e9a424a1b811dd7ac1102
Arian <arian.kulmer@web.de>
2022-06-22 16:31:14 +0200

    zeus: Bring back face detection libs
    Change-Id: I50af944e9398eef352f72f4729999f8d0c556837
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -58,6 +58,10 @@ blob_fixups: blob_fixups_user_type = {
     .clear_symbol_version('remote_handle64_invoke')
     .clear_symbol_version('remote_handle64_open')
     .clear_symbol_version('remote_register_buf_attr'),
+    ('vendor/lib64/hw/camera.qcom.so', 'vendor/lib64/libFaceDetectpp-0.5.2.so', 'vendor/lib64/libfacedet.so'): blob_fixup()
+        .binary_regex_replace(b'libmegface.so', b'libfacedet.so')
+        .binary_regex_replace(b'libMegviiFacepp-0.5.2.so', b'libFaceDetectpp-0.5.2.so')
+        .binary_regex_replace(b'megviifacepp_0_5_2_model', b'facedetectpp_0_5_2_model'),
 }
 
 module = ExtractUtilsModule(
