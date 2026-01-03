PixelOS-Devices/device_xiaomi_zeus

zeus: PixelOS-ify
Signed-off-by: Rasenkai <rasenkai99@gmail.com>

AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_zeus.mk
+    $(LOCAL_DIR)/aosp_zeus.mk

PixelOS-Devices/android_device_xiaomi_zeus

Change-Id: Ic411b9cd6ca6f3ed607d6335ee9c47e3d7210118

AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_zeus.mk
+    $(LOCAL_DIR)/custom_zeus.mk