4d0e7527d9838664e80ce3752c0d6a6283bfded0
Arian <arian.kulmer@web.de>
2026-03-24 10:13:21 +0100

    zeus: Update blobs and firmware from OS3.0.3.0.VLBMIXM
    Change-Id: I5f1b7b419280889f2a10bc68dd6cfff7a3bebe2a

--- custom_zeus.mk
+++ custom_zeus.mk
@@ -21,8 +21,8 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2201122G
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="zeus_global-user 15 AQ3A.250226.002 OS3.0.2.0.VLBMIXM release-keys" \
-    BuildFingerprint=Xiaomi/zeus_global/zeus:15/AQ3A.250226.002/OS3.0.2.0.VLBMIXM:user/release-keys \
+    BuildDesc="zeus_global-user 15 AQ3A.250226.002 OS3.0.3.0.VLBMIXM release-keys" \
+    BuildFingerprint=Xiaomi/zeus_global/zeus:15/AQ3A.250226.002/OS3.0.3.0.VLBMIXM:user/release-keys \
     DeviceProduct=zeus \
     SystemName=zeus_global
 
